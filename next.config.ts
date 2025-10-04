a3272ed7bcc8bc0eb2e8537b8b59218b83b13ae8
Faisal9029 <faisalsharif39@yahoo.com>
2025-10-05 04:33:33 +0500

    nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 import CaseSensitivePathsPlugin from 'case-sensitive-paths-webpack-plugin';
 
 const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
   webpack(config) {
     config.plugins.push(new CaseSensitivePathsPlugin());
     return config;
